906b0bd27ac5d35b3961cb9273ce75799b6b8f91
Carson Kompon <carsokompo@gmail.com>
2023-09-02 15:41:13 -0400

    Applied gmod UI

--- code/ui/SandboxHud.cs
+++ code/ui/SandboxHud.cs
@@ -1,5 +1,7 @@
 ﻿using Sandbox;
 using Sandbox.UI;
+using Sandbox.UI.Construct;
+using CarsonK;
 
 [Library]
 public partial class SandboxHud : HudEntity<RootPanel>
@@ -17,9 +19,61 @@ public partial class SandboxHud : HudEntity<RootPanel>
 		RootPanel.AddChild<KillFeed>();
 		RootPanel.AddChild<Scoreboard<ScoreboardEntry>>();
 		RootPanel.AddChild<Health>();
-		RootPanel.AddChild<InventoryBar>();
+		RootPanel.AddChild<GmodInventoryBar>();
 		RootPanel.AddChild<CurrentTool>();
-		RootPanel.AddChild<SpawnMenu>();
+		RootPanel.AddChild<GmodSpawnMenu>();
 		RootPanel.AddChild<Crosshair>();
+
+		RootPanel.StyleSheet.Load("/ui/gmod/gmod.scss");
+
+		for(int z=0; z<RootPanel.ChildrenCount; z++)
+		{
+			var child = RootPanel.GetChild(z);
+			switch(child.GetType().Name)
+			{
+				case "Health":
+					child.Add.Label("HEALTH", "health-header");
+					break;
+				
+				case "Chat":
+
+					for(int i=0; i<child.ChildrenCount; i++)
+					{
+						var chatChild = child.GetChild(i);
+						if(chatChild is TextEntry chatEntry)
+						{
+							var entryPanel = child.Add.Panel("textentry-container");
+							entryPanel.Add.Label("Say :", "textentry-say");
+							entryPanel.AddChild(chatEntry);
+						}
+					}
+					break;
+				
+				case "SpawnMenu":
+					var newSpawnMenu = RootPanel.AddChild<GmodSpawnMenu>();
+					RootPanel.SetChildIndex(newSpawnMenu, RootPanel.GetChildIndex(child));
+					child.Delete(true);
+					break;
+				
+				case "InventoryBar":
+					var newInventoryBar = RootPanel.AddChild<GmodInventoryBar>();
+					RootPanel.SetChildIndex(newInventoryBar, RootPanel.GetChildIndex(child));
+					child.Delete(true);
+					break;
+
+				default:
+					if(child.GetType().Name.Contains("Scoreboard"))
+					{
+						child.Add.Label(Game.Server.GameTitle, "scoreboard-header");
+					}
+					else
+					{
+						Log.Info("Unknown type: " + child.GetType().Name);
+					}
+					break;
+			}
+		}
+
+
 	}
 }
